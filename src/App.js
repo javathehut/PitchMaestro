e15394f80aafab8c05a75d12cac51b8c0a398bd3
Angelique Lazarus <javathehut@users.noreply.github.com>
2017-10-16 13:47:03 -0400

    change wording and add media query

--- src/App.js
+++ src/App.js
@@ -72,8 +72,9 @@ class App extends Component {
       <div>
         <div className="page-header">
           <h1 className="text-center">Pitch Maestro <br />
-            <small className="small">An ear trainer for musicians and the musically inclined</small></h1>
-            <h3 className="text-center roboto">Click anywhere you see "<span className="glyphicon glyphicon-chevron-down"></span>" for instructions!</h3>
+            <small className="small">An ear trainer for the musically inclined and musically interested!</small>
+            <small className="small2">A musical ear trainer for all!</small></h1>
+            <h3 className="text-center roboto">Click any "<span className="glyphicon glyphicon-chevron-down"></span>" for instructions!</h3>
         </div>
         <div className="row">
           <Maestro keyIndex={this.state.keyIndex} chordIndex={this.state.chordIndex} changeChord={this.changeChord} changeKey={this.changeKey} testing={this.state.testing} toggle={this.toggle} collapse={this.state.collapse}/>
